689820164569529f99648a23f03a134abffea561
KYIV\Roman_Hryhorenko <roman.hryhorenko@mckesson.com>
2021-09-13 15:18:39 +0300

    homework

---
+++ src/main/java/com/configuration/db/JdbcDriverSetUp.java
@@ -0,0 +1,76 @@
+package com.configuration.db;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class JdbcDriverSetUp {
+
+    String DBURL = "jdbc:mysql://localhost:3306/homework";
+    String USERNAME = "root";
+    String PASSWORD = "password123";
+    Connection connection;
+    Statement statement;
+    ResultSet resultSet;
+
+    @Before
+    public void dbDriverSetUp() throws SQLException {
+        try {
+            connection = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+            statement = connection.createStatement();
+        } catch (SQLException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void selectFromDB() {
+        try {
+            String query = "SELECT * FROM USER";
+            resultSet = statement.executeQuery(query);
+            Assertions.assertEquals("test_user1\n"
+                                    + "test_user2\n"
+                                    + "test_user3", "test_user1\n"
+                                                    + "test_user2\n"
+                                                    + "test_user3", "Select is not match");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("username"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void selectFromDataBaseWithJoin() {
+        try {
+            String query = "SELECT * FROM user INNER JOIN scientist;";
+            resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("firstname"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @After
+    public void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/test/java/com/test/db/DBTests.java
@@ -0,0 +1,19 @@
+package com.test.db;
+
+import com.configuration.db.JdbcDriverSetUp;
+import org.junit.Test;
+
+import java.sql.ResultSet;
+
+public class DBTests {
+
+    @Test
+    public void selectFromDataBse() {
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
